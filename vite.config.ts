4bee24f1e42624336a31c999fa88377e6794940f
pravoslavniy <56929559+pravoslavniy@users.noreply.github.com>
2025-06-05 16:18:51 +0300

    fix: base to ./

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pravoslavniy/pravoslavniy.github.io/',
+  base: './',
   build: {
     rollupOptions: {
       output: {

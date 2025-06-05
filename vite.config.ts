78c8baf39d1536315c6dac91efbca511ed9975e0
pravoslavniy <56929559+pravoslavniy@users.noreply.github.com>
2025-06-05 15:51:35 +0300

    Prepare for deploy

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/pravoslavniy/pravoslavniy.github.io/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
